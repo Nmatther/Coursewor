e4d7580057ecd3ff4a5ec812a60c457df8c6b37c
Nmatther <nmatther@icloud.com>
2024-02-08 14:29:56 -0800

    finished

---
+++ React-Form/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Authenticate from './components/Authenitcate'
+import SignUpForm from './components/SignUpForm'
+
+export default function App(){
+  const [token, setToken] = useState(null);
+
+  return(
+    <>
+    
+    <SignUpForm token={token} setToken={setToken} />
+    <Authenticate token={token} setToken={setToken} />
+    </>
+  );
+}
+
+
---
+++ React-Form/src/components/SignUpForm.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+export default function SignUpForm({setToken}) {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("")
+
+    async function handleSubmit(event){
+        event.preventDefault();
+        
+        try {
+            const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup',
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({username, password}),
+                    
+                    })
+                    const result = await response.json();
+                    setToken(result.token);
+
+                    setPassword("")
+                    setUsername("")
+                }
+            
+        catch (error){
+            setError(error.message);
+        }
+    }
+
+        return (
+            <>
+                <h2>Sign Up!</h2>
+                <form onSubmit={handleSubmit}>
+                <label>
+                    Username: <input value={username} onChange={(e) => setUsername(e.target.value)} />
+                </label>
+                <label>
+                    Password: <input value={password} onChange={(e) => setPassword(e.target.value)} />
+                </label>
+                <button>Submit</button>
+                </form>
+            </>
+        );
+}
